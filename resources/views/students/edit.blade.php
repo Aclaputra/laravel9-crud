a31ba9f1f4d6151a26a62fe7fd698bf2112c6a12
Aclaputra <muhamadacla22@gmail.com>
2022-05-03 11:25:25 +0700

    added modified resources/views/students/edit.blade.php

--- resources/views/students/edit.blade.php
+++ resources/views/students/edit.blade.php
@@ -1,22 +1,22 @@
 @extends('students.layout')
 @section('content')
- 
 <div class="card">
-  <div class="card-header">Students Page</div>
+  <div class="card-header">Edit Page</div>
   <div class="card-body">
       
-      <form action="{{ url('student') }}" method="post">
+      <form action="{{ url('dashboard/' .$students->id) }}" method="post">
         {!! csrf_field() !!}
+        @method("PATCH")
+        <input type="hidden" name="id" id="id" value="{{$students->id}}" id="id" />
         <label>Name</label></br>
-        <input type="text" name="name" id="name" class="form-control"></br>
+        <input type="text" name="name" id="name" value="{{$students->name}}" class="form-control"></br>
         <label>Address</label></br>
-        <input type="text" name="address" id="address" class="form-control"></br>
+        <input type="text" name="address" id="address" value="{{$students->address}}" class="form-control"></br>
         <label>Mobile</label></br>
-        <input type="text" name="mobile" id="mobile" class="form-control"></br>
-        <input type="submit" value="Save" class="btn btn-success"></br>
+        <input type="text" name="mobile" id="mobile" value="{{$students->mobile}}" class="form-control"></br>
+        <input type="submit" value="Update" class="btn btn-success"></br>
     </form>
-   
+  
   </div>
 </div>
- 
 @stop
